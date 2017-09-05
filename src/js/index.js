02604e5d35a95cb0c52a661d714cf371e1f0dc0f
cris1948 <krz.fraus@gmail.com>
2017-09-05 23:28:03 +0200

    'test'

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,7 @@ let Search = {
 
 window.onload = function () {
   let searchInput = document.getElementById('search');
+  console.log('test')
   searchInput.addEventListener('input', (evt) => {
     _.delay((evt) => {
       Search.listenerAction(evt.target.value)
