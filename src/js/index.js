339e25fbfe2b1db454be3255be2dc32f95cf69f8
cris1948 <krz.fraus@gmail.com>
2017-09-05 23:29:13 +0200

    - add lodash, recognize movieDB API

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,6 @@ let Search = {
 
 window.onload = function () {
   let searchInput = document.getElementById('search');
-  console.log('test')
   searchInput.addEventListener('input', (evt) => {
     _.delay((evt) => {
       Search.listenerAction(evt.target.value)
