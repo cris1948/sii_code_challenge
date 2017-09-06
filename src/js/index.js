3432ef19351175d1f2b27f3b907d276a5705bd08
cris1948 <krz.fraus@gmail.com>
2017-09-06 21:07:39 +0200

    - rewrite make request to promises - get request_token - get quest_session_id - start writing render function

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,77 @@
 const MOVIEDB_API_KEY = 'fff2f288005425652515f2361d8b1964';
-const MOVIEDB_BASE_URL = 'http://api.themoviedb.org/3';
-const MOVIEDB_IMAGES_URL = 'http://image.tmdb.org/t/p';
+const MOVIEDB_BASE_URL = 'https://api.themoviedb.org/3';
+const MOVIEDB_IMAGES_URL = 'https://image.tmdb.org/t/p';
+const MOVIEDB_AUTHENTICATE_URL = 'https://www.themoviedb.org/authenticate/'
+
+const methods = {
+  getToken: {url: 'authentication/token/new', type: 'GET'},
+  createSession: {url: 'authentication/session/new', type: 'GET'},
+  createGuestSession: {url: 'authentication/guest_session/new', type: 'GET'},
+  searchMovie: {url: 'search/movie', type: 'GET'}
+};
 
 let MovieDB = {
-  listenerAction (text) {
-    console.log(text, this)
-  },
-  makeRequest (requestedUrl, method, data, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+  guestSessionId: null,
+  results: null,
+  makeRequest (method, data) {
+    return new Promise(function (resolve, reject) {
+      let xhr = new XMLHttpRequest();
+      let queryString;
 
-    xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === this.DONE) {
-        console.log(this.responseText);
-      } else {
-        console.log(this.readyState);
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+          resolve(JSON.parse(this.response))
+        }
+      });
+
+      if (methods[method]['type'] === 'GET') {
+        queryString = MovieDB.createQueryString(data);
       }
+      xhr.onerror = reject;
+      xhr.open(methods[method]['type'], `${MOVIEDB_BASE_URL}/${methods[method]['url']}${queryString}`, true);
+
+      xhr.send(data || {});
     });
-
-    xhr.open(method, `${MOVIEDB_BASE_URL}/${requestedUrl}/${MOVIEDB_API_KEY}`);
-
-    xhr.send(data);
+  },
+  createQueryString (data) {
+    let string = `?api_key=${MOVIEDB_API_KEY}`;
+    if (data) {
+      Object.keys(data).forEach((key) => {
+        console.log(data[key]);
+        string += `&${key}=${data[key]}`
+      });
+    }
+    return string;
+  },
+  listenerAction (textQuery) {
+    this.guestSessionId ? this.searchMovie(textQuery) : this.authenticateUser(textQuery)
+  },
+  searchMovie (textQuery, page) {
+    this.makeRequest('searchMovie', {query: textQuery, page: 1}).then((response) => {
+      this.results = response
+    })
+  },
+  authenticateUser (textQuery) {
+    this.makeRequest('getToken').then((response) => {
+      return this.makeRequest('createGuestSession');
+    }).then((response) => {
+      this.guestSessionId = response.guest_session_id;
+    }).then(() => {
+      this.searchMovie(textQuery)
+    }).catch((error) => {
+      console.log(error)
+    })
+  },
+  renderResults () {
+    // let container = document.getElementById('results');
   }
 };
 
 window.onload = function () {
   let searchInput = document.getElementById('search');
-  searchInput.addEventListener('input', (evt) => {
-    _.delay((evt) => {
-      MovieDB.listenerAction(evt.target.value)
-    }, 1000, evt)
-  })
+  searchInput.addEventListener('input', _.debounce(function (evt) {
+    MovieDB.listenerAction(evt.target.value)
+  }, 1000))
 };
 
 
