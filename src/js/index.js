079f4cca001fa7ecc0f143c65b2f95360d453694
cris1948 <krz.fraus@gmail.com>
2017-09-06 19:23:37 +0200

    - add const, - add makeRequest method

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,26 @@
-let Search = {
+const MOVIEDB_API_KEY = 'fff2f288005425652515f2361d8b1964';
+const MOVIEDB_BASE_URL = 'http://api.themoviedb.org/3';
+const MOVIEDB_IMAGES_URL = 'http://image.tmdb.org/t/p';
+
+let MovieDB = {
   listenerAction (text) {
     console.log(text, this)
+  },
+  makeRequest (requestedUrl, method, data, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+      } else {
+        console.log(this.readyState);
+      }
+    });
+
+    xhr.open(method, `${MOVIEDB_BASE_URL}/${requestedUrl}/${MOVIEDB_API_KEY}`);
+
+    xhr.send(data);
   }
 };
 
@@ -8,7 +28,7 @@ window.onload = function () {
   let searchInput = document.getElementById('search');
   searchInput.addEventListener('input', (evt) => {
     _.delay((evt) => {
-      Search.listenerAction(evt.target.value)
+      MovieDB.listenerAction(evt.target.value)
     }, 1000, evt)
   })
 };
